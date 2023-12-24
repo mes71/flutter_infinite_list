046eb5a1f078178411a90d947e4da6f227f56bd5
Mr.Mojtaba <mr.mojtaba47@gamil.com>
2023-12-24 16:31:42 +0330

    feat => Presentation Layer

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'posts/view/posts_page.dart';
+
 
 class App extends MaterialApp {
   const App({super.key}) : super(home: const PostsPage());
--- lib/posts/bloc/post_bloc.dart
+++ lib/posts/bloc/post_bloc.dart
@@ -6,11 +6,9 @@ import 'package:bloc_concurrency/bloc_concurrency.dart';
 import 'package:equatable/equatable.dart';
 import 'package:http/http.dart' as http;
 import 'package:stream_transform/stream_transform.dart';
-
 import '../models/models.dart';
 
 part 'post_event.dart';
-
 part 'post_state.dart';
 
 const throttleDuration = Duration(milliseconds: 100);
@@ -26,14 +24,15 @@ class PostBloc extends Bloc<PostEvent, PostState> {
   final http.Client httpClient;
 
   PostBloc({required this.httpClient}) : super(const PostState()) {
-    on<PostEvent>((event, emit) {
-      on<PostFetched>(_onPostFetched,
-          transformer: throttleDroppable(throttleDuration));
-    });
+    on<PostFetched>(_onPostFetched,
+        transformer: throttleDroppable(throttleDuration));
+
   }
 
-  FutureOr<void> _onPostFetched(PostFetched event,
-      Emitter<PostState> emit) async {
+  Future<void> _onPostFetched(
+    PostFetched event,
+    Emitter<PostState> emit,
+  ) async {
     if (state.hasReachedMax) return;
     try {
       if (state.status == PostStatus.initial) {
@@ -45,11 +44,10 @@ class PostBloc extends Bloc<PostEvent, PostState> {
       emit(posts.isEmpty
           ? state.copyWith(hasReachedMax: true)
           : state.copyWith(
-        status: PostStatus.success,
-        posts: List.of(state.posts)
-          ..addAll(posts),
-        hasReachedMax: false,
-      ));
+              status: PostStatus.success,
+              posts: List.of(state.posts)..addAll(posts),
+              hasReachedMax: false,
+            ));
     } catch (_) {
       emit(state.copyWith(status: PostStatus.failure));
     }
@@ -77,6 +75,3 @@ class PostBloc extends Bloc<PostEvent, PostState> {
     throw Exception('error fetching posts');
   }
 }
-
-
-
